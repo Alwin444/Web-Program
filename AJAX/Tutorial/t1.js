aac06b5855879e6a6b4d940740db578ce501f9c5
Alwin George <150264978+Alwin444@users.noreply.github.com>
2025-05-26 13:38:02 +0530

    Create t1.js

---
+++ AJAX/Tutorial/t1.js
@@ -0,0 +1,18 @@
+fetch("t1.php")
+.then(response => response.text())
+.then(data => {
+    const arr = data.split('|');
+    let Income = arr[0];
+    let Expense = arr[1];
+
+    let Pr_design = document.getElementsByClassName("rec1")[0];
+    let Ex_design = document.getElementsByClassName("rec1")[1];
+
+    Pr_design.style.color = "green";
+    Pr_design.style.textAlign = "center";
+    Pr_design.innerText = "Income: " + Income;
+
+    Ex_design.style.color = "red";
+    Ex_design.style.textAlign = "center";
+    Ex_design.innerText = "Expense: " + Expense;
+});
